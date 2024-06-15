d5bdc6b4b7b8847263ee4055996dd6e48ab7ce89
JairoARojasR <jairoalexisrr21@gmail.com>
2024-06-15 16:58:45 -0500

    VentaRouteActualizado

--- routes/ventaRoute.js
+++ routes/ventaRoute.js
@@ -11,17 +11,9 @@ const {
 } = require("../middleware/authMiddleware"); 
 
 const router = express.Router(); 
-//const { authJwt } = require('../middlewares');
-//const { verifyToken, checkPermission } = authJwt;
 router.post("/", createVenta);
-//router.post("/", authMiddleware, isRole(["Admin" , "Empleado"]), createCategoria);
-//router.put("/:id", [authJwt.verifyToken, authJwt.checkPermission('65b93de42d4d1eab56a0825d')], updateCategoria);
 router.get("/:id", getVenta);
 router.get("/", getAllVentas);
 
-// router.delete("/:id",authMiddleware,  isRole(["Admin"]), deleteCategoria);
-// router.get("/:id", getCategoria);
-// router.put("/:id", authMiddleware,  isRole(["Empleado"]), updateCategoria);
-// router.get("/", getallCategoria);
 
 module.exports = router;
