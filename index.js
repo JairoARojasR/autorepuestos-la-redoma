e424144b7acd69234070163e3066c0e975733d27
JairoARojasR <jairoalexisrr21@gmail.com>
2024-06-20 00:34:37 -0500

    Se ha actualizado index.js

--- index.js
+++ index.js
@@ -31,30 +31,21 @@ app.listen(PORT, () => {
 });  
 
 
+
+
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://ayrlaredoma.netlify.app');
-  //res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin','https://ayrlaredoma.netlify.app');
+    //res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
 
-  // You can also configure other headers as needed
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
-
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin','https://ayrlaredoma.netlify.app');
-//     //res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
-
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     // Handle preflight requests sino
-//     if (req.method === 'OPTIONS') {
-//       res.sendStatus(200);
-//     } else {
-//       next();
-//     }
-//   });
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    // Handle preflight requests sino
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  });
 
 //app.use('/public/confirm', pruebita)
 app.use('/api/categoria', categoriaRouter);
